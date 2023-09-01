d81643ef564268285ceb72e3e8830e43abfdaab9
fluddsskark <huzaifayousuf888@hotmail.com>
2023-09-01 20:25:52 +0500

    v2

--- sound.js
+++
@@ -1,7 +0,0 @@
-const audioOne = new Audio("sounds/confirm.mp3");
-const audioTwo = new Audio("sounds/select.mp3");
-const link = document.querySelectorAll("a.link"); //Returns a list with ALL the elements containing the link class
-for(element of link){
-  element.addEventListener("click", audioOne.play);
-  element.addEventListener("mouseover", audioTwo.play);
-};
